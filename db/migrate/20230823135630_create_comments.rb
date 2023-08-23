c394df2573d61c72ea89ebc8beb82e8402824a09
ClintonYounge <clintonyounge18@gmail.com>
2023-08-23 16:19:51 -0400

    Add references to migrations

--- db/migrate/20230823135630_create_comments.rb
+++ db/migrate/20230823135630_create_comments.rb
@@ -1,7 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.integer :author_id
+      t.references :user, null: false, foreign_key: { to_table: :users }
+      t.references :post, null: false, foreign_key: { to_table: :posts }
       t.integer :post_id
       t.text :text
       t.timestamps
