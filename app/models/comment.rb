0a8f075e7c91934da0b24ab76c91209f71d5908b
ClintonYounge <clintonyounge18@gmail.com>
2023-08-24 18:26:05 -0400

    Update models to add methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
+
+  after_commit :increment_comments_count, on: :create
+
+  private
+
+  def increment_comments_count
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
 class Like < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
+
+  after_commit :increment_likes_count, on: :create
+
+  private
+
+  def increment_likes_count
+    post.increment!(:likes_counter)
+  end
 end
