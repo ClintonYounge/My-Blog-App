3e08c0dde52f2a1c56d263a3b5d8b7cec6b1e941
ClintonYounge <clintonyounge18@gmail.com>
2023-08-29 01:51:15 -0400

    Remove duplicate route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
     resources :posts, only: [:index, :show]
   end
 
-  get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
   get '/users/:user_id/posts', to: 'posts#index'
   get '/users/:user_id/posts/:id', to: 'posts#show'
