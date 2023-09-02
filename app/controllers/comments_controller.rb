24f2359047bb5f09272ca453481884e31b98bf2e
ClintonYounge <clintonyounge18@gmail.com>
2023-09-02 17:05:59 -0400

    Add new method to CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,20 @@
 class CommentsController < ApplicationController
   before_action :set_post
 
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
   def create
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
     @comment.author = current_user
 
     if @comment.save
-      redirect_to user_post_path(@user, @post), notice: 'Comment was successfully created.'
+      redirect_to user_post_path(@user, @post), notice: 'Comment added successfully'
     else
-      redirect_to user_post_path(@user, @post), alert: 'Comment creation failed.'
+      render 'new'
     end
   end
 
