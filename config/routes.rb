ad57437fb66cc6da37714d05f4248f9f59f6e8c4
ClintonYounge <clintonyounge18@gmail.com>
2023-09-13 17:40:17 -0400

    Update to fully functional code

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    passwords: 'users/passwords',
-  }
+  devise_for :users
+  
   root 'users#index'
 
   resources :users, only: [:index, :show] do
