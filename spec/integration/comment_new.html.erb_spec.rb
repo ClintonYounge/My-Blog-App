0de1a6cedb123c86209051886bd17e22cc1a95d5
ClintonYounge <clintonyounge18@gmail.com>
2023-09-13 22:11:09 -0400

    Refactor integration tests

--- spec/integration/comment_new.html.erb_spec.rb
+++ spec/integration/comment_new.html.erb_spec.rb
@@ -2,30 +2,27 @@ require 'rails_helper'
 
 RSpec.describe 'Comment new', type: :feature do
   before do
-    @user1 = User.create(name: 'Cjay', bio: 'Cjay bio', photo: 'https://th.bing.com/th/id/R.df818c6db2fe74a4230ccc863663226e?rik=Bt%2fD%2bcd60%2bv0KQ&pid=ImgRaw&r=0')
-    @user2 = User.create(name: 'Max', bio: 'Max bio', photo: 'https://th.bing.com/th/id/R.df818c6db2fe74a4230ccc863663226e?rik=Bt%2fD%2bcd60%2bv0KQ&pid=ImgRaw&r=0')
-    @user3 = User.create(name: 'Ahmed', bio: 'Ahmed bio', photo: 'https://th.bing.com/th/id/R.df818c6db2fe74a4230ccc863663226e?rik=Bt%2fD%2bcd60%2bv0KQ&pid=ImgRaw&r=0')
+    @user1 = User.create(name: 'Cjay', bio: 'Cjay bio', email: 'cjay@example.com', password: 'password', posts_counter: 0, photo: 'https://th.bing.com/th/id/R.df818c6db2fe74a4230ccc863663226e?rik=Bt%2fD%2bcd60%2bv0KQ&pid=ImgRaw&r=0')
+    @user2 = User.create(name: 'Max', bio: 'Max bio', email: 'max@example.com', password: 'password', posts_counter: 0, photo: 'https://th.bing.com/th/id/R.df818c6db2fe74a4230ccc863663226e?rik=Bt%2fD%2bcd60%2bv0KQ&pid=ImgRaw&r=0')
+    @user3 = User.create(name: 'Ahmed', bio: 'Ahmed bio', email: 'ahmed@example.com', password: 'password', posts_counter: 0, photo: 'https://th.bing.com/th/id/R.df818c6db2fe74a4230ccc863663226e?rik=Bt%2fD%2bcd60%2bv0KQ&pid=ImgRaw&r=0')
     @post1 = Post.create(author: @user1, title: 'My First Post', text: 'This is my post content.')
     @post2 = Post.create(author: @user2, title: 'My Second Post', text: 'This is my second post content.')
     @post3 = Post.create(author: @user3, title: 'My Third Post', text: 'This is my third post content.')
+    @current_user = User.create(name: 'Test User', email: 'test@example.com', password: 'password')
+    login_as(@current_user, scope: :user)
   end
 
   it 'Can see the new comment page' do
-    visit '/users'
-    click_link 'Cjay'
-    expect(page).to have_current_path("/users/#{User.first.id}")
-    click_link 'See all posts'
-    expect(page).to have_current_path("/users/#{User.first.id}/posts")
+    visit user_posts_path(@user1)
     click_link 'My First Post'
-    expect(page).to have_current_path("/users/#{User.first.id}/posts/#{Post.first.id}")
     click_link 'Add Comment'
-    expect(page).to have_current_path("/users/#{User.first.id}/posts/#{Post.first.id}/comments/new")
+    expect(page).to have_current_path(new_user_post_comment_path(@user1, @post1))
     expect(page).to have_content('Comment')
     expect(page).to have_button('Add Comment')
   end
 
   it 'Can create a comment' do
-    visit "/users/#{User.first.id}/posts/#{Post.first.id}/comments/new"
+    visit new_user_post_comment_path(@user1, @post1)
     fill_in 'Comment', with: 'This is my comment.'
     click_button 'Add Comment'
     expect(page).to have_content('This is my comment.')
--- spec/integration/user_show.html.erb_spec.rb
+++ spec/integration/user_show.html.erb_spec.rb
@@ -2,18 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'User show page', type: :feature do
   before do
-    @user1 = User.create(name: 'Cjay', bio: 'Cjay bio', photo: 'https://th.bing.com/th/id/R.df818c6db2fe74a4230ccc863663226e?rik=Bt%2fD%2bcd60%2bv0KQ&pid=ImgRaw&r=0')
-    @user2 = User.create(name: 'Max', bio: 'Max bio', photo: 'https://th.bing.com/th/id/R.df818c6db2fe74a4230ccc863663226e?rik=Bt%2fD%2bcd60%2bv0KQ&pid=ImgRaw&r=0')
-    @user3 = User.create(name: 'Ahmed', bio: 'Ahmed bio', photo: 'https://th.bing.com/th/id/R.df818c6db2fe74a4230ccc863663226e?rik=Bt%2fD%2bcd60%2bv0KQ&pid=ImgRaw&r=0')
+    @user1 = User.create(name: 'Cjay', bio: 'Cjay bio', email: 'cjay@example.com', password: 'password', posts_counter: 0, photo: 'https://th.bing.com/th/id/R.df818c6db2fe74a4230ccc863663226e?rik=Bt%2fD%2bcd60%2bv0KQ&pid=ImgRaw&r=0')
+    @user2 = User.create(name: 'Max', bio: 'Max bio', email: 'max@example.com', password: 'password', posts_counter: 0, photo: 'https://th.bing.com/th/id/R.df818c6db2fe74a4230ccc863663226e?rik=Bt%2fD%2bcd60%2bv0KQ&pid=ImgRaw&r=0')
+    @user3 = User.create(name: 'Ahmed', bio: 'Ahmed bio', email: 'ahmed@example.com', password: 'password', posts_counter: 0, photo: 'https://th.bing.com/th/id/R.df818c6db2fe74a4230ccc863663226e?rik=Bt%2fD%2bcd60%2bv0KQ&pid=ImgRaw&r=0')
+    @current_user = User.create(name: 'Test User', email: 'test@example.com', password: 'password')
+    login_as(@current_user, scope: :user)
   end
 
   it 'I can see the user profile picture' do
-    visit "/users/#{User.first.id}"
+    visit user_path(@user1)
     expect(page).to have_css("img[src*='https://th.bing.com/th/id/R.df818c6db2fe74a4230ccc863663226e?rik=Bt%2fD%2bcd60%2bv0KQ&pid=ImgRaw&r=0']")
   end
 
   it 'I can see the user name' do
-    visit "/users/#{User.first.id}"
+    visit user_path(@user1)
     expect(page).to have_content('Cjay')
   end
 
@@ -24,12 +26,12 @@ RSpec.describe 'User show page', type: :feature do
     Post.create(author: @user3, title: 'My First Post', text: 'This is my post content.')
     Post.create(author: @user3, title: 'My First Post', text: 'This is my post content.')
     Post.create(author: @user3, title: 'My First Post', text: 'This is my post content.')
-    visit "/users/#{User.first.id}"
+    visit user_path(@user1)
     expect(page).to have_content('Number of posts: 1')
   end
 
   it 'I can see the user bio' do
-    visit "/users/#{User.first.id}"
+    visit user_path(@user1)
     expect(page).to have_content('Cjay bio')
   end
 
@@ -39,7 +41,7 @@ RSpec.describe 'User show page', type: :feature do
     third_post = Post.create(author: @user1, title: 'My Third Post', text: 'This is my third post content.')
     fourth_post = Post.create(author: @user1, title: 'My Fourth Post', text: 'This is my fourth post content.')
     fifth_post = Post.create(author: @user1, title: 'My Fifth Post', text: 'This is my fifth post content.')
-    visit "/users/#{User.first.id}"
+    visit user_path(@user1)
     expect(page).not_to have_content(first_post.text)
     expect(page).not_to have_content(second_post.text)
     expect(page).to have_content(third_post.text)
@@ -48,19 +50,21 @@ RSpec.describe 'User show page', type: :feature do
   end
 
   it 'I can see a button that lets me view all of the user posts' do
-    visit "/users/#{User.first.id}"
+    visit user_path(@user1)
     expect(page).to have_link('See all posts')
   end
+
   it 'When I click a user post, it redirects me to that post show page' do
     Post.create(author: @user1, title: 'My First Post', text: 'This is my post content.')
-    visit "/users/#{User.first.id}"
+    visit user_path(@user1)
     click_link 'My First Post'
-    expect(page).to have_current_path("/users/#{User.first.id}/posts/#{Post.first.id}")
+    expect(page).to have_current_path(user_post_path(@user1, Post.first))
   end
+
   it 'When I click the See all posts button, it redirects me to the user posts index page' do
     Post.create(author: @user1, title: 'My First Post', text: 'This is my post content.')
-    visit "/users/#{User.first.id}"
+    visit user_path(@user1)
     click_link 'See all posts'
-    expect(page).to have_current_path("/users/#{User.first.id}/posts")
+    expect(page).to have_current_path(user_posts_path(@user1))
   end
 end
